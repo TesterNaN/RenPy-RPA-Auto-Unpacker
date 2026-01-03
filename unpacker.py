7f92be9e3dbe4a17d085b6184063d914b4bab96e
TesterNaN <88355571+TesterNaN@users.noreply.github.com>
2026-01-03 17:27:35 +0800

    Update and rename unpacker_universal.py to unpacker.py

--- unpacker.py
+++ unpacker.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+RenPy-RPA-Auto-Unpacker - 全自动的 Ren'Py RPA 解包工具
+Copyright (C) 2025 TesterNaN
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import os
 import re
 import os, glob
@@ -328,3 +349,4 @@ with open('core.py', 'a', encoding='utf-8') as f:
     f.write(code_part_2+'\n')
 from core import main
 main()
+
